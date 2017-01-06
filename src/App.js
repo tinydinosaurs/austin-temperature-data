3545d7c86903a773d38aea7ce4d6e776017efbb6
Dana <dana.lachman@gmail.com>
2017-01-05 22:26:15 -0600

    wiped out boilerplate code and made a pretty.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  handleCLick() {
+    console.log("bored yet?");
+    // gonna put a fetch in here and see what kind of data I get back from NOAA.
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Goodbye Blue Monday</h2>
+          <button onClick={this.handleCLick}>click me</button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
