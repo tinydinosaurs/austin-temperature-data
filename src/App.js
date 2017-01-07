8ae25c579042a09bc5e31c588bfb28abbebea815
Dana <dana.lachman@gmail.com>
2017-01-06 19:38:32 -0600

    successfully grabbed a data object from Climate Data Online

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+import $ from 'jquery';
+import config from '../public/config';
 
 class App extends Component {
   handleCLick() {
     console.log("bored yet?");
     // gonna put a fetch in here and see what kind of data I get back from NOAA.
+    $.ajax({
+      url: "https://www.ncdc.noaa.gov/cdo-web/api/v2/datacategories",
+      type: "GET",
+      headers:{
+        token: config.CDN_TOKEN
+      },
+      success: function(response) {
+        console.log("success!", response);
+      },
+      error: function(msg) {
+        console.log("error 😿", msg)
+      }
+      })
+
   }
 
   render() {
